1a99d235b0861bae1927eae727b8fdbaeb64b3e9
eva yoon <evayryoon@gmail.com>
2025-10-18 15:18:29 -0700

    history

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -17,4 +17,18 @@
 	];
 </script>
 
-<div></div>
+<div class="space-y-2">
+	{#each [1, 2, 3] as v}
+		<div class="flex items-center rounded-xl bg-gray-400 p-2">
+			<div class="bg-pink-300">*</div>
+			<div class="flex-1 bg-green-200">
+				<div>[group name...] [code #]</div>
+				<div>10/2</div>
+				<div>Met <input type="checkbox" /></div>
+			</div>
+			<div>
+				<div class="bg-pink-300">avatars</div>
+			</div>
+		</div>
+	{/each}
+</div>
