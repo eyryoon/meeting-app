5feca6f0bea1d201a59f0e6fd0ececf3d7a1efac
eva yoon <evayryoon@gmail.com>
2025-10-19 16:45:09 -0700

    update

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -17,13 +17,15 @@
 	];
 </script>
 
-<div class="space-y-2">
+<div class="mt-4 space-y-5">
 	{#each [1, 2, 3] as v}
-		<div class="flex items-center rounded-xl bg-gray-400 p-2">
+		<div
+			class="hover-shadow-lg flex cursor-pointer items-center rounded-xl border border-gray-500 p-2"
+		>
 			<div class="bg-pink-300">*</div>
-			<div class="flex-1 bg-green-200">
-				<div>[group name...] [code #]</div>
-				<div>10/2</div>
+			<div class="flex-1">
+				<div><span class="bold-text text-2xl">[group name...]</span> [code #]</div>
+				<div class="mt-2">Created 10/2</div>
 				<div>Met <input type="checkbox" /></div>
 			</div>
 			<div>
